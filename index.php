5da4844c162004462d11d95d32e92baf8ca853a1
Romain Dauby <romain.dauby@gmail.com>
2014-04-14 19:58:55 +0200

    Initialize bootstrap, jQuery and Modernizr

--- index.php
+++ index.php
@@ -1,1 +1,22 @@
-Hello world !
+<!DOCTYPE html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>FollowforFollow</title>
+        <meta name="description" content="Free instagram followers : you follow, they follow you back. No subscription, very easy !">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/vendor/modernizr-2.7.1.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p>You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <p>Hello world!</p>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+    </body>
+</html>
