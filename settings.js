6e865227a62f209e9b87a967e12ecbbadc76c043
Romain Dauby <romain.dauby@gmail.com>
2014-06-09 09:36:20 +0200

    Refacto settings using var

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var APISettings = {
-    clientId : "6d8d64686f04484bb54cf7bbf8ddcd15",
-    redirectUri : "http://followforfollow.dev/token.html",
-    scope : ["likes", "comments", "relationships", "basic"],
-    apiUri : "https://instagram.com/oauth/authorize/?client_id=6d8d64686f04484bb54cf7bbf8ddcd15&redirect_uri=http://followforfollow.dev/token.html&response_type=token&scope=likes+comments+relationships+basic",
-    apiBaseUri : "https://api.instagram.com/v1"
-}
+
+var APISettings = {};
+APISettings.clientId = "419658066049438eb0bbd3cd7d726b9a";
+APISettings.redirectUri = "http://followforfollow.dev/token.html";
+APISettings.scope = ["likes", "comments", "relationships", "basic"];
+APISettings.apiUri = "https://instagram.com/oauth/authorize/?client_id=" + APISettings.clientId + "&redirect_uri=" + APISettings.redirectUri + "&response_type=token&scope=likes+comments+relationships+basic";
+APISettings.apiBaseUri = "https://api.instagram.com/v1";
